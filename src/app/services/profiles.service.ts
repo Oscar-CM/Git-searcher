3dd90e17442f649bd3814fd0581c8db1789cdc77
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-15 10:44:42 +0300

    corrected a bug in the services file

--- src/app/services/profiles.service.ts
+++ src/app/services/profiles.service.ts
@@ -8,6 +8,7 @@ export class ProfileService {
     private client_secret = 'd1c186c6373f96571c0bfcf76b84e4dc6fd0c15a';
     constructor(private http: HttpClient) {}
 
+    
     getUser() {
         return this.http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret);
     }
@@ -19,5 +20,6 @@ export class ProfileService {
     updateUserName(username: string) {
         this.username = username;
     }
+
 
 }
