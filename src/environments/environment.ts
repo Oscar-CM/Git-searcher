65415969ad46f91ffd838199b93d86f6f8085a82
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-15 09:32:35 +0300

    initiam commit of my other project aspects

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "https://api.github.com/users/",
+  apikey: "?access_token=e2346f9a4e865151bc41932faea8b3b4f060716f",
+  apiRepokey: "/repos?access_token=e2346f9a4e865151bc41932faea8b3b4f060716f"
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -14,3 +16,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// e2346f9a4e865151bc41932faea8b3b4f060716f
