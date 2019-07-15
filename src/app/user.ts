2c38de5d03648f8f8318a85be1f0307df8d3a2ed
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-15 09:17:38 +0300

    added the user class

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  constructor(
+    public Username: string,
+    public Repositories: string,
+    public Followers: Number,
+    public Following: Number,
+    public Email: string,
+    public MemberSince: number
+  ) {}
+}
